a967180c783104cf3bc9044c4f693a728e27ff21
rigo72 <mattia.gori27@gmail.com>
2024-10-03 09:35:25 +0200

    Commit 1

---
+++ esercizio11.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $linkBase = "https://xkcd.com";
+        $numLink = rand(2,5);
+        for($i=1;$i<$numLink;$i++){
+            echo "<a href = '{$linkBase}/{$i}'>{$linkBase}/{$i}</a>";
+            echo "<br>";
+        }
+    ?>
+</body>
+</html>
